02b6f6722225c229740ec11e45e7b7bf9db9e963
Ren <s1583620@ed.ac.uk>
2019-07-24 15:16:45 +0100

    Add bash sh

---
+++ phLP_experiments.sh
@@ -0,0 +1,10 @@
+python train_multiphase.py --phLP 10 --path Reset_both_26
+python train_multiphase.py --phLP 20 --path Reset_both_27
+python train_multiphase.py --phLP 40 --path Reset_both_28
+python train_multiphase.py --phLP 80 --path Reset_both_29
+python train_multiphase.py --phLP 120 --path Reset_both_30
+python train_multiphase.py --phLP 160 --path Reset_both_31
+python train_multiphase.py --phLP 200 --path Reset_both_32
+python train_multiphase.py --phLP 300 --path Reset_both_33
+python train_multiphase.py --phLP 400 --path Reset_both_34
+python train_multiphase.py --phLP 800 --path Reset_both_35
