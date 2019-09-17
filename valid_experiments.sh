78185766763364a43752a1721b4c5cce3f778420
夏叶子 <xiayezi@xiayezideMacBook-Air.local>
2019-09-17 14:21:26 +0800

    Add scripts of validation

---
+++ valid_experiments.sh
@@ -0,0 +1,4 @@
+python train_multiphase.py --path Valid_0 --valid_num 0
+python train_multiphase.py --path Valid_8 --valid_num 8
+python train_multiphase.py --path Valid_16 --valid_num 16
+python train_multiphase.py --path Valid_32 --valid_num 32
