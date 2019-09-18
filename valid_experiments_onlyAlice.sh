3d1385376914ad93495509f4dad80c5bb68bc0c1
夏叶子 <xiayezi@xiayezideMacBook-Air.local>
2019-09-18 14:19:23 +0800

    Validation alice only

---
+++ valid_experiments_onlyAlice.sh
@@ -0,0 +1,4 @@
+python train_multiphase.py --path Valid_0_onlyAlice --valid_num 0
+python train_multiphase.py --path Valid_8_onlyAlice --valid_num 8
+python train_multiphase.py --path Valid_16_onlyAlice --valid_num 16
+python train_multiphase.py --path Valid_32_onlyAlice --valid_num 32
